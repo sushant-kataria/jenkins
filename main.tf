130334e61d38eda63f9d184df4f60910f6297992
sushantkataria <sushantkat@gmail.com>
2025-03-26 23:46:16 -0700

    made changes to main file

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
