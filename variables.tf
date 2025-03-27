040b6261a09774dec54df9b2624d6636b95b9d5c
sushantkataria <sushantkat@gmail.com>
2025-03-26 23:50:10 -0700

    made changes to variable file

--- variables.tf
+++ variables.tf
@@ -1,4 +1,23 @@
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+
